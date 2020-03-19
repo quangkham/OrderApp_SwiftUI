b94c61c38e31cb0eca07394f0af4955bf930d780
Nan Mauk Quang Kham <nanmaukquangkham@techfun-myanmar.com>
2020-03-19 10:17:04 +0630

    Pusd a new view. andd presenting view with NavigationView in List and sheet

---
+++ OrderApp/OrderApp/Presentation/ContextMenuView.swift
@@ -0,0 +1,21 @@
+//
+//  ContextMenuView.swift
+//  OrderApp
+//
+//  Created by TechFun on 19/03/2020.
+//  Copyright © 2020 TechFunMyanmar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContextMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContextMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContextMenuView()
+    }
+}
---
+++ OrderApp/OrderApp/Presentation/PresentViewbySheet.swift
@@ -0,0 +1,21 @@
+//
+//  PresentViewbySheet.swift
+//  OrderApp
+//
+//  Created by TechFun on 19/03/2020.
+//  Copyright © 2020 TechFunMyanmar. All rights reserved.
+//
+
+import SwiftUI
+
+struct PresentViewbySheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PresentViewbySheet_Previews: PreviewProvider {
+    static var previews: some View {
+        PresentViewbySheet()
+    }
+}
